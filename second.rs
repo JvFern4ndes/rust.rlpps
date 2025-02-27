fcef65008464fcbd534262fc08cf51702b7f4279
JvFern4ndes <jvfernandes.contato@gmail.com>
2025-02-27 17:40:41 -0300

    feat: some new tests with rust

--- second.rs
+++ second.rs
@@ -1,7 +1,21 @@
 const PI:f32 = 3.14;
 static mut VARIAVEL_GLOBAL:u8 = 1;
 
-fn main() {
+fn sombra() {
+    let a = 123;
+
+    {
+        let b = 456;
+        println!("dentro, b = {}", b);
+
+        let a = 777;
+        println!("dentro, a = {}", a);
+    }
+
+    println!("fora, a = {}", a);
+}
+
+fn escopo() {
     println!("PI = {}", PI);
 
     unsafe {
@@ -13,9 +27,14 @@ fn main() {
     let decimal:f32 = 2.5;
     println!("decimal = {}", decimal);
 
-    let mut booleana:bool = false;
+    let booleana:bool = false;
     println!("Booleana = {}, Tamanho booleana = {}", booleana, std::mem::size_of_val(&booleana));
 
     let letra:char = 'C';
     println!("Tamanho do char = {}", std::mem::size_of_val(&letra));
+}
+
+fn main() {
+    escopo();
+    sombra();
 }
