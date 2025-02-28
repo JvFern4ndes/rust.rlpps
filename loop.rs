020c503d59b794fa669388f1064ee96cf8890fec
JvFern4ndes <jvfernandes.contato@gmail.com>
2025-02-27 21:43:31 -0300

    feat: try to use loops with rust

---
+++ loop.rs
@@ -0,0 +1,28 @@
+fn main() {
+    multiplicador();
+}
+
+fn multiplicador() {
+    let multiplicador:u8 = 8;
+    let mut contador:u8 = 0;
+
+    while contador < 10 {
+        contador += 1;
+
+        if contador == 5 {
+            continue;
+        }
+        
+        println!("{} x {} = {}", multiplicador, contador, multiplicador * contador);
+    }
+
+    contador = 0;
+    loop {
+        contador += 1;
+        println!("{} x {} = {}", multiplicador, contador, multiplicador * contador);
+
+        if contador == 10 {
+            break;
+        }
+    }
+}
